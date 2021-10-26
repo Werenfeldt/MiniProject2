f9a87f0af0b799db787353c688ccf359f67b55dd
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-10-26 15:48:27 +0200

    More client (#2)
    * Kinda works
    
    * now with join messages
    
    * The chat woooorks

--- Client/main.go
+++ Client/main.go
@@ -3,49 +3,190 @@ package main
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	pd "MiniProject2/Chitty_Chat"
-	"time"
+	// "os"
+	"MiniProject2/Chitty_Chat"
+	//"time"
 
 	"google.golang.org/grpc"
 )
 
-const (
-	address     = "localhost:8080"
-	defaultName = "chittyChat"
-)
-
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	fmt.Println("Enter Server IP:Port ::: ")
+	reader := bufio.NewReader(os.Stdin)
+	serverID, err := reader.ReadString('\n')
+
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Printf("Failed to read from console :: %v", err)
+	}
+	serverID = strings.Trim(serverID, "\r\n")
+
+	log.Println("Connecting : " + serverID)
+
+	//connect to grpc server
+	conn, err := grpc.Dial(serverID, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Faile to conncet to gRPC server :: %v", err)
 	}
 	defer conn.Close()
-	c := pd.NewChitty_ChatClient(conn)
 
-	//input
-	//var inputMessage string
 
-	// Taking input from user
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		line := scanner.Text()
+	//call ChatService to create a stream
+	client := Chitty_Chat.NewChitty_ChatClient(conn)
 
-		// Contact the server and print out its response.
-		// Id := defaultName
-		// if len(os.Args) > 1 {
-		// 	name = os.Args[1]
-		// }
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
-		r, err := c.BroadcastMessage(ctx, &pd.BroadcastRequest{Message: line})
-		if err != nil {
-			log.Fatalf("could not greet: %v", err)
+	//message stream
+	stream, err := client.BroadcastMessage(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to call ChatService :: %v", err)
+	}
+
+	//status stream
+	// statusStream, err := client.StatusMessage(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("Failed to call ChatService :: %v", err)
+	// }
+
+	// implement communication with gRPC server
+	ch := clienthandle{stream: stream}
+	//st := statushandle{statusStream: statusStream}
+	ch.clientConfig()
+	// st.statusConfig(ch)
+	// go st.SendStatus()
+	// go st.RecieveStatus()
+	go ch.sendMessage()
+	go ch.receiveMessage()
+
+	//blocker
+	bl := make(chan bool)
+	//blo := make(chan bool)
+	<-bl
+	//<-blo
+
+	//!!!!Status besked skal stå heroppe så den bliver sendt før messages bliver sendt.
+	//!! lav eventuelt stadig en StatusConfig metode
+
+}
+
+//clienthandle and stream for message
+type clienthandle struct {
+	stream     Chitty_Chat.Chitty_Chat_BroadcastMessageClient
+	clientName string
+	//HasStatus bool
+}
+
+//Statushandle and streat for status updates
+// type statushandle struct {
+// 	statusStream  		Chitty_Chat.Chitty_Chat_StatusMessageClient
+// 	clientName string
+// }
+
+
+//sets name for client and status 
+func (ch *clienthandle) clientConfig() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Your Name : ")
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf(" Failed to read from console :: %v", err)
+	}
+
+	ch.clientName = strings.Trim(name, "\r\n")
+	//ch.HasStatus = false
+
+	ch.SendStatus()
+}
+
+// func (st *statushandle) statusConfig(ch clienthandle) {
+// 	st.clientName = strings.Trim(ch.clientName, "\r\n")
+// }
+
+//sends status to server that 
+func (ch *clienthandle) SendStatus() {	
+
+		clientMessageBox := &Chitty_Chat.BroadcastRequest{
+			Name: ch.clientName,
+			
+			Message: "Has joined the Chat",
+			//Status: ch.HasStatus,
 		}
-		log.Printf("The server: %s", r.GetMessage())
+	
+		err := ch.stream.Send(clientMessageBox)
+	
+		if err != nil {
+			log.Printf("Error while sending message to server :: %v", err)
+		}
 
+}
+
+//recieve status if others have joined
+// func (st *statushandle) RecieveStatus() {
+// 	for{
+// 		mssg, err := st.statusStream.Recv()
+// 		if err != nil {
+// 			log.Printf("Error in receiving status: %v from server :: %v",mssg, err)
+// 		}
+
+// 		//should probably return the message if the name is the same. 
+// 		 if(mssg.Name != st.clientName){
+// 		 	fmt.Printf("StatusChannel:  %s : %s",mssg.Name, mssg.Message)
+// 		}	
+
+// 	}
+// }
+
+//send message
+func (ch *clienthandle) sendMessage() {
+
+	for {
+
+		reader := bufio.NewReader(os.Stdin)
+		clientMessage, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf(" Failed to read from console :: %v", err)
+		}
+		clientMessage = strings.Trim(clientMessage, "\r\n")
+		
+		
+		clientMessageBox := &Chitty_Chat.BroadcastRequest{
+			Name: ch.clientName,
+			
+			Message: clientMessage,
+			//Status: true,
+		}
+
+		err = ch.stream.Send(clientMessageBox)
+	
+	
+		if err != nil {
+			log.Printf("Error while sending message to server :: %v", err)
+		}
+
+	}
+
+}
+
+//receive message
+func (ch *clienthandle) receiveMessage() {
+
+	
+	for {
+		mssg, err := ch.stream.Recv()
+		if err != nil {
+			log.Printf("Error in receiving message from server :: %v", err)
+		}
+
+		if(mssg.Name == ""){
+			fmt.Printf("%s \n", mssg.Message)
+		} else {
+			fmt.Printf("%s : %s \n",mssg.Name, mssg.Message)
+		}
+		//print message to console
 	}
 }
--- Server/main.go
+++ Server/main.go
@@ -1,39 +1,48 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
+	"os"
 
-	pb "MiniProject2/Chitty_Chat"
+	"MiniProject2/Chitty_Chat"
 
 	"google.golang.org/grpc"
 )
 
-const (
-	port = ":8080"
-)
-
-// server is used to implement helloworld.GreeterServer.
-type server struct {
-	pb.UnimplementedChitty_ChatServer
-}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) BroadcastMessage(ctx context.Context, in *pb.BroadcastRequest) (*pb.BroadcastResponse, error) {
-	log.Printf("Received: %v", in.GetMessage())
-	return &pb.BroadcastResponse{Message: "Message receive"}, nil
-}
-
 func main() {
-	lis, err := net.Listen("tcp", port)
+
+Port := os.Getenv("PORT")
+	if Port == "" {
+		Port = "8080" //default Port set to 5000 if PORT is not set in env
+	}
+
+	//init listener
+	listen, err := net.Listen("tcp", ":"+Port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Could not listen @ %v :: %v", Port, err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterChitty_ChatServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	log.Println("Listening @ : " + Port)
+
+	//gRPC server instance
+	grpcserver := grpc.NewServer()
+
+
+	//register ChatService
+	cs := Chitty_Chat.ChatServer{}
+	Chitty_Chat.RegisterChitty_ChatServer(grpcserver,&cs)
+	
+	//grpc listen and serve
+	err = grpcserver.Serve(listen)
+	if err != nil {
+		log.Fatalf("Failed to start gRPC Server :: %v", err)
+	} else {
+		
 	}
+
+	
+	//cs.Start()
+	
 }
+
+
